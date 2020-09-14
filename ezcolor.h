31a513227b9fb5fc5cdbb785b5a95d67821b35d7
Greg Landry <gjl@cypress.com>
2020-09-14 11:42:01 -0400

    Update ezcolor.h

--- ezcolor.h
+++ ezcolor.h
@@ -6,12 +6,10 @@ typedef enum
   RED           = CY_RGB_LED_COLOR_RED,
   GREEN         = CY_RGB_LED_COLOR_GREEN,
   BLUE          = CY_RGB_LED_COLOR_BLUE,
-  YELLOW        = CY_RGB_LED_COLOR_YELLOW,
-  PURPLE        = CY_RGB_LED_COLOR_PURPLE,
-  WHITE         = CY_RGB_LED_COLOR_WHITE,
-  ORANGE        = (255 << CY_RGB_LED_RED_POS) | (165 << CY_RGB_LED_GREEN_POS) | (0   << CY_RGB_LED_BLUE_POS),
-  PINK          = (255 << CY_RGB_LED_RED_POS) | (105 << CY_RGB_LED_GREEN_POS) | (180 << CY_RGB_LED_BLUE_POS),
+  ORANGE        = (255 << CY_RGB_LED_RED_POS) | (5   << CY_RGB_LED_GREEN_POS) | (0   << CY_RGB_LED_BLUE_POS),
+  PINK          = (255 << CY_RGB_LED_RED_POS) | (10  << CY_RGB_LED_GREEN_POS) | (30  << CY_RGB_LED_BLUE_POS),
   LIGHT_BLUE    = (173 << CY_RGB_LED_RED_POS) | (216 << CY_RGB_LED_GREEN_POS) | (230 << CY_RGB_LED_BLUE_POS)
+
 } color_t;
 
 cy_rslt_t init_colors( void );
